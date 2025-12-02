39357844af2d1c0830b4511b4e367221a7aa7fca
murphy-afk <bardarenoemi@gmail.com>
2025-12-02 14:34:24 +0100

    feat: genre useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-
-
+  const [genre, setGenre] = useState('');
   return (
     <>
       <div className="container">
         <h1>Movies</h1>
         <label htmlFor="genre-select" className='form-label'>Filtra per genre</label>
-        <select name="genre" id="genre-slect" className='form-control'>
+        <select name="genre" id="genre-select" className='form-control' 
+        onChange={(event) => setGenre(event.target.value)}>
           {movies.map((movie, index) =>
             <option value={movie.genre} key={index}>{movie.genre}</option>
           )}
