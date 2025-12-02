07079bdd77e9d13347826ea0666c779b4819ed22
murphy-afk <bardarenoemi@gmail.com>
2025-12-02 15:20:28 +0100

    feat: all movies show if no genre is selected and styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,33 @@ import './App.css'
 
 function App() {
   const [genre, setGenre] = useState('');
-  const [movies, setMovies] = useState(movieList)
-
+  const [movies, setMovies] = useState(movieList);
+  const [movieNum, setMovieNum] = useState(1);
   useEffect(() => {
+    console.log("I'm happening");
     const filteredMovies = movieList.filter(movie => movie.genre === genre);
-    setMovies(filteredMovies)
+    setMovies(filteredMovies);
+    setMovieNum(filteredMovies.length);
+    if (genre === '') {
+      setMovies(movieList)
+    }
   }, [genre])
 
   return (
     <>
-      <div className="container">
-        <h1>Movies</h1>
-        <label htmlFor="genre-select" className='form-label'>Filtra per genre</label>
-        <select name="genre" id="genre-select" className='form-control' 
-        onChange={(event) => {setGenre(event.target.value)}}>
-          {movieList.map((movie, index) =>
-            <option value={movie.genre} key={index}>{movie.genre}</option>
-          )}
-        </select>
-        <div className='movie-container'>
+      <div className="container w-75 border my-4 card bg-info-subtle">
+        <h1 className='text-center py-3'>Movies</h1>
+        <div className="form-floating">
+          <select name="genre" id="genre-select" className='form-select' aria-label='Filtra per genre'
+            onChange={(event) => { setGenre(event.target.value) }}>
+            {movieList.map((movie, index) =>
+              <option value={movie.genre} key={index}>{movie.genre}</option>
+            )}
+          </select>
+          <label htmlFor="genre-select">Filtra per genre</label>
+        </div>
+        <div className='movie-container mt-5'>
+          {genre != '' && <p className='border-bottom border-dark'>Film trovati: {movieNum}</p>}
           {movies.map((movie, index) =>
             <p key={index}>{movie.title}</p>
           )}
